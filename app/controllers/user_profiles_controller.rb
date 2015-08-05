fd9faea66266ff75b9562c861f89415321755e74
khattori <khattori@aiming-inc.biz>
2015-08-05 12:48:21 +0900

    プロフィール更新ページを作成

---
+++ app/controllers/user_profiles_controller.rb
@@ -0,0 +1,26 @@
+class UserProfilesController < ApplicationController
+  def show
+    @user_profile = UserProfile.find(params[:id])
+  end
+
+  def edit
+    @user_profile = UserProfile.find(params[:id])
+  end
+
+  def update
+    @user_profile = UserProfile.find(params[:id])
+
+    result = @user_profile.update_attributes(user_profile_params)
+    if result
+      redirect_to user_profile_path(@user_profile)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_profile_params
+    params.require(:user_profile).permit(:first_name, :last_name)
+  end
+end
---
+++ spec/features/user_profiles_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature '登録者のプロフィール', type: :feature do
+
+  feature 'プロフィールの表示' do
+    let!(:user_profile) { FactoryGirl.create :user_profile }
+
+    background do
+      visit user_profile_path(user_profile)
+    end
+
+    scenario 'プロフィールが表示されること' do
+      expect(page).to have_content user_profile.last_name
+      expect(page).to have_content user_profile.first_name
+    end
+  end
+
+
+  feature 'プロフィールの編集' do
+    let!(:user_profile) { FactoryGirl.create :user_profile }
+    let!(:edited_first_name) { 'edited_first_name' }
+    let!(:edited_last_name) { 'edited_last_name' }
+
+    background do
+      visit edit_user_profile_path(user_profile)
+    end
+
+    scenario 'プロフィールが編集できる' do
+
+      fill_in 'user_profile_first_name', with: edited_first_name
+      fill_in 'user_profile_last_name', with: edited_last_name
+
+      click_on 'Update User profile'
+
+      expect(current_path).to eq user_profile_path(user_profile)
+
+      expect(page).to have_content edited_first_name
+      expect(page).to have_content edited_last_name
+    end
+  end
+
+end
