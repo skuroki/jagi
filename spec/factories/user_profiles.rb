9e91439a7a4fa1b7d4a0e3d0083204fff2083c4d
yinaura <yinaura@aiming-inc.com>
2015-08-06 16:05:38 +0900

    FactoryGirl で forgery を使うようにした。

--- spec/factories/user_profiles.rb
+++ spec/factories/user_profiles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user_profile do
-    first_name "John"
-    last_name  "Doe"
+    first_name     { Forgery('name').first_name }
+    last_name      { Forgery('name').last_name  }
   end
 end
