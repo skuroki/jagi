78908af171567df2603a8e10437a78e10ebe6f9c
yinaura <yinaura@aiming-inc.com>
2015-08-11 18:25:15 +0900

    Deviseの設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_one :user_profile
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :user_profiles, only: [:show, :edit, :update]
 end
