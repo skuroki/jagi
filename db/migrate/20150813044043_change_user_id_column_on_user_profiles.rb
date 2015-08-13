2261e0a2597bb379da5328b25e4d123b467f4472
yinaura <yinaura@aiming-inc.com>
2015-08-13 13:19:25 +0900

    モデル・migrationを作成した

--- db/migrate/20150813044043_change_user_id_column_on_user_profiles.rb
+++ db/migrate/20150813044043_change_user_id_column_on_user_profiles.rb
@@ -1,7 +1,7 @@
 class ChangeUserIdColumnOnUserProfiles < ActiveRecord::Migration
   def change
+    change_column_null :user_profiles, :user_id, false
     remove_column :user_profiles, :first_name
     remove_column :user_profiles, :last_name
-    change_column_null :user_profiles, :user_id, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150813044043) do
 
+  create_table "answers", force: :cascade do |t|
+    t.integer "user_profile_id",    limit: 4,   null: false
+    t.integer "to_user_profile_id", limit: 4,   null: false
+    t.boolean "correct",                        null: false
+    t.string  "answer",             limit: 255
+  end
+
   create_table "user_profiles", force: :cascade do |t|
     t.integer  "user_id",     limit: 4,   null: false
     t.datetime "created_at",              null: false
