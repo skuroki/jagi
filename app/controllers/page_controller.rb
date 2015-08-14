df633929cc42330179177ae0e703a23da0b2bc69
yinaura <yinaura@aiming-inc.com>
2015-08-14 15:31:43 +0900

    クイズの回答履歴が記録・表示されるように

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,5 @@
 class PageController < ApplicationController
   def index
-
+    @start_button_enabled = UserProfile.new.answer_user.present?
   end
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,4 +3,16 @@ class UserProfile < ActiveRecord::Base
   delegate :name, to: :user
   validates :user_id, presence: true, numericality: true, uniqueness: true
   validates :answer_name, length: { maximum: 30 }
+
+  def answer_user
+    self.class.all.sample
+  end
+
+  def total_correct
+    Answer.where(correct: true, user_profile_id: self.id).count
+  end
+
+  def total_incorrect
+    Answer.where(correct: false, user_profile_id: self.id).count
+  end
 end
