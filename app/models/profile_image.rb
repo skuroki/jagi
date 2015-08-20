a48ae518a7ff054a0b856595d778cfa6c90b4f7f
yinaura <yinaura@aiming-inc.com>
2015-08-20 10:12:49 +0900

    アップロード機能を追加した

--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -1,7 +1,21 @@
 class ProfileImage < ActiveRecord::Base
   belongs_to :user_profile
+
   validates :user_profile_id, presence: true, numericality: true
   validates :image, presence: true
-  mount_uploader :image, ImageUploader
   validates :situation, presence: true
- end
+
+  mount_uploader :image, ImageUploader
+
+  def self.situations
+    [:normal, :correct, :incorrect]
+  end
+
+  def self.save_image (params)
+    return unless params[:image]
+
+    profile_image = ProfileImage.find_or_create_by(user_profile_id: params[:user_profile_id] , situation: params[:situation])
+    profile_image.update(params)
+  end
+end
+
