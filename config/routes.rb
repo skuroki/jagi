cada3b052dc699462b0a4cad162b74abc33ba773
yinaura <yinaura@aiming-inc.com>
2015-08-21 21:54:34 +0900

    routingを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+  resources :ranking, only: [:index]
   resources :user_profiles, only: [:edit, :update]
   resource :page, only: [:index]
   resource :quiz, only: [:show] do
