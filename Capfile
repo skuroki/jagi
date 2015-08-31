937807cf1eac1a5cd72af0c7def2d19aa89a386a
hattori0407 <khattori@aiming-inc.biz>
2015-08-31 15:49:04 +0900

    capistranoのタスクでunicornを起動/再起動できるようにした

--- Capfile
+++ Capfile
@@ -4,7 +4,6 @@ require 'capistrano/rbenv'
 require 'capistrano/bundler'
 require 'capistrano/rails'
 
-# FIXME: うまく動かない。あとで修正します。
-# require 'capistrano3/unicorn'
+require 'capistrano3/unicorn'
 
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
+set :rails_env, :production
 
 server 'jagi', user: 'jagi', roles: %w{web app db}
 
@@ -11,26 +12,7 @@ set :ssh_options, {
   forward_agent: true,
 }
 
-# FIXME: capistrano+unicon による起動・再起動がうまくいかない。
-# tmo/sokets tmp/pids にシンボリックリンクを貼ると エラー。jagiユーザーで実行すると何故かうまくいかない。あとで修正します。
-
-# set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
-# set :unicorn_rack_env, -> { 'production' }
-# set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
-# set :unicorn_roles, -> { :jagi }
-
-# FIXME: capistrano+unicon による起動・再起動がうまくいかない。あとで修正します。
-
-# namespace :deploy do
-#   task :start do
-#     invoke 'unicorn:start'
-#   end
-
-#   task :stop do
-#     invoke 'unicorn:stop'
-#   end
-
-#   task :restart do
-#     invoke 'unicorn:legacy_restart'
-#   end
-# end
+set :unicorn_roles, :web
+set :unicorn_rack_env, :production
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
