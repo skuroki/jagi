894cc927b289302ee4d4c5b8b25ceaaa5b9a0e79
hattori0407 <khattori@aiming-inc.biz>
2015-08-31 16:38:43 +0900

    不要な設定を削除

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,10 +5,7 @@ server 'jagi', user: 'jagi', roles: %w{web app db}
 
 set :linked_dirs, %w{tmp/pids tmp/sockets public/uploads}
 
-paths = [ENV["HOME"]+'/.ssh/id_rsa']
-paths.unshift ENV["PRIVATE_KEY_PATH"] if ENV.has_key? 'PRIVATE_KEY_PATH'
 set :ssh_options, {
-  keys: paths,
   forward_agent: true,
 }
 
