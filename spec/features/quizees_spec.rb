e4c601d3e59479565d7ffb0a2a3d57fc9c7449b1
yinaura <yinaura@aiming-inc.com>
2015-10-02 11:03:16 +0900

    brush up feature test
    - Changed language, Write in Japanese to English. Make result of 'rspec -fd' appropriate.
      featureのテストを英語に変えて、ネストを整えた。英語のほうが正しい文脈を考えやすい。 rspec -fd でなるべく適切な文脈がとれるようにした。
    
    ---------------------------------------
    Result of `rspec -fd`
    ---------------------------------------
    
    Quiz
      Start
        display starting form
        When the user clicks start button
          the quiz starts
      Answer
        To many questions continuity
          all questions have each different users
          all questions are shuffled
        To one question
          display answer form
          User challenges to a game and get result
            display be answered user profile
            When the answer was incorrect
              display losing message
              display total incorrect num
            When the answer was correct
              display total correct num
              display winning message

--- spec/features/quizees_spec.rb
+++ spec/features/quizees_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
-feature 'クイズ', type: :feature do
+feature 'Quiz', type: :feature do
   include_context 'login_as_user'
 
-  feature 'クイズを始める' do
+  feature 'Start' do
     before do
       visit new_quiz_path
     end
 
-    scenario 'クイズ開始フォームが表示される' do
+    scenario 'display starting form' do
       expect(page).to have_select('joined_year')
       expect(page).to have_select('project_id')
       expect(page).to have_select('group_id')
-      # expect(page).to have_field('review_mode')
       expect(page).to have_button I18n.t('quiz.new.start_quiz')
     end
 
-    context 'クイズを始めるボタンをクリックしたとき' do
-      scenario 'クイズが開始される' do
+    context 'When the user clicks start button' do
+      scenario 'the quiz starts' do
         click_button I18n.t('quiz.new.start_quiz')
         expect(current_path).to eq question_quiz_path
       end
@@ -25,7 +24,7 @@ feature 'クイズ', type: :feature do
   end
 
   # FIXME: ビューのテストにしかなってないのでなおす
-  feature 'クイズを進める' do
+  feature 'Answer' do
     let!(:users) { FactoryGirl.create_list :user_profile, 30 }
     let!(:question_user) { FactoryGirl.create :user_profile }
 
@@ -34,94 +33,96 @@ feature 'クイズ', type: :feature do
       click_button I18n.t('quiz.new.start_quiz')
     end
 
-    scenario '回答入力フォームが表示される' do
-      expect(page).to have_field('answer_name')
-      expect(page).to have_button I18n.t('quiz.question.submit')
-    end
-
-    feature 'クイズに回答する' do
-      before do
-        allow_any_instance_of(Quiz).to receive(:last_question).and_return(question_user)
+    feature 'To one question' do
+      scenario 'display answer form' do
+        expect(page).to have_field('answer_name')
+        expect(page).to have_button I18n.t('quiz.question.submit')
       end
 
-      scenario '問題となったユーザのプロフィールが表示される' do
-        fill_in 'answer_name', with: question_user.answer_name
-        click_button I18n.t('quiz.question.submit')
+      feature 'User challenges to a game and get result' do
+        before do
+          allow_any_instance_of(Quiz).to receive(:last_question).and_return(question_user)
+        end
 
-        expect(page).to have_content question_user.user.name
-        expect(page).to have_content question_user.answer_name
-        expect(page).to have_content question_user.detail
-        # expect(page).to have_content gender
-        # expect(page).to have_content joined_year
-        # expect(page).to have_content group_name
-        # expect(page).to have_content project_name
-      end
+        scenario 'display question user profile' do
+          fill_in 'answer_name', with: question_user.answer_name
+          click_button I18n.t('quiz.question.submit')
 
-      feature 'to many questions continuity' do
-        let(:question_user_ids) {
-          questions = []
+          expect(page).to have_content question_user.user.name
+          expect(page).to have_content question_user.answer_name
+          expect(page).to have_content question_user.detail
+          # expect(page).to have_content gender
+          # expect(page).to have_content joined_year
+          # expect(page).to have_content group_name
+          # expect(page).to have_content project_name
+        end
 
-          10.times do
-            questions.push find('#question_image')['data-user-id'].to_i
+        context 'When the answer was correct' do
+          before do
+            allow_any_instance_of(Quiz).to receive(:last_result).and_return(:win)
+          end
 
+          scenario 'display winning message' do
             fill_in 'answer_name', with: question_user.answer_name
             click_button I18n.t('quiz.question.submit')
-            click_button I18n.t('quiz.result.next')
+
+            expect(page).to have_content I18n.t('quiz.result.correct')
           end
-          questions
-        }
 
-        scenario 'questions are shuffled' do
-          expect(question_user_ids).not_to eq question_user_ids.sort
+          scenario 'display total correct num' do
+            fill_in 'answer_name', with: question_user.answer_name
+            click_button I18n.t('quiz.question.submit')
+
+            click_button I18n.t('quiz.result.next')
+            expect(page).to have_css('#total_correct', I18n.t('quiz.show.correct'))
+            expect(page).to have_css('#total_correct', text: '1')
+          end
         end
 
-        scenario 'different user is found per 1 question' do
-          expect(question_user_ids.uniq.size).to eq question_user_ids.size
-        end
-     end
+        context 'When the answer was incorrect' do
+          before do
+            allow_any_instance_of(Quiz).to receive(:last_result).and_return(:lose)
+          end
 
-      context '正解だった場合' do
-        before do
-          allow_any_instance_of(Quiz).to receive(:last_result).and_return(:win)
-        end
+          scenario 'display losing message' do
+            fill_in 'answer_name', with: question_user.answer_name
+            click_button I18n.t('quiz.question.submit')
 
-        scenario 'お祝いのメッセージが表示される' do
-          fill_in 'answer_name', with: question_user.answer_name
-          click_button I18n.t('quiz.question.submit')
+            expect(page).to have_content I18n.t('quiz.result.incorrect')
+          end
 
-          expect(page).to have_content I18n.t('quiz.result.correct')
-        end
+          scenario 'display total incorrect num' do
+            fill_in 'answer_name', with: question_user.answer_name
+            click_button I18n.t('quiz.question.submit')
 
-        scenario '正解数の合計が表示される' do
-          fill_in 'answer_name', with: question_user.answer_name
-          click_button I18n.t('quiz.question.submit')
-
-          click_button I18n.t('quiz.result.next')
-          expect(page).to have_css('#total_correct', I18n.t('quiz.show.correct'))
-          expect(page).to have_css('#total_correct', text: '1')
+            click_button I18n.t('quiz.result.next')
+            expect(page).to have_css('#total_incorrect', I18n.t('quiz.show.incorrect'))
+            expect(page).to have_css('#total_incorrect', text: '1')
+          end
         end
       end
+    end
 
-      context '不正解だった場合' do
-        before do
-          allow_any_instance_of(Quiz).to receive(:last_result).and_return(:lose)
-        end
+    feature 'To many questions continuity' do
+      let(:question_user_ids) {
+        questions = []
 
-        scenario 'お悔やみのメッセージが表示される' do
+        10.times do
+          questions.push find('#question_image')['data-user-id'].to_i
+
           fill_in 'answer_name', with: question_user.answer_name
           click_button I18n.t('quiz.question.submit')
-
-          expect(page).to have_content I18n.t('quiz.result.incorrect')
-        end
-
-        scenario '不正解数の合計が表示される' do
-          fill_in 'answer_name', with: question_user.answer_name
-          click_button I18n.t('quiz.question.submit')
-
           click_button I18n.t('quiz.result.next')
-          expect(page).to have_css('#total_incorrect', I18n.t('quiz.show.incorrect'))
-          expect(page).to have_css('#total_incorrect', text: '1')
         end
+        questions
+      }
+
+      scenario 'all questions are shuffled' do
+        expect(question_user_ids).not_to eq question_user_ids.sort
+      end
+
+      scenario 'all questions have each different users' do
+        expect(question_user_ids.uniq.size).to eq question_user_ids.size
       end
     end
   end
